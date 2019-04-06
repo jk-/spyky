Fix test for connection calculation

spyky/neuron/ifneuron.py
@@ -39,7 +39,7 @@ class IFNeuron(NeuronLayer):
 
         super().tick(v_incoming)
 
-    def reset(self) -> None:
+    def reset(self) -> NoReturn:
         super().reset()
         self.refractor_count = np.zeros(self.shape)
         self.v = self.v_rest * np.ones(self.shape)

test/test_connection.py
@@ -34,12 +34,12 @@ class TestConnection:
         bias = np.array([1, 1, 1])
 
         connection = Connection(
-            NeuronLayer((1, 1)),
-            NeuronLayer((1, 1)),
+            NeuronLayer((1, 3)),
+            NeuronLayer((1, 3)),
             weights=weights,
             bias=bias,
         )
         spikes = np.array([0, 1, 0])
         weights_calc = connection.calculate(spikes)
 
-        assert all([a == b for a, b in zip(weights_calc, [2.0, 2.0, 2.0])])
+        assert all([a == b for a, b in zip(weights_calc[0], [2.0, 2.0, 2.0])])